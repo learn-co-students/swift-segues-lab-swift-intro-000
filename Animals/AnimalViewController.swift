326586afb5490323b052af1a976a06e01ff3be4e
RGero215 <rgero215@gmail.com>
2016-10-29 07:15:21 -0400

    Done.

--- Animals/AnimalViewController.swift
+++ Animals/AnimalViewController.swift
@@ -9,4 +9,50 @@
 import UIKit
 
 class AnimalViewController: UIViewController {
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblSubTitle: UILabel!
+    
+    var animal : String?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let animal = animal {
+            let emoji = switchAnimal(animal: animal)
+            lblSubTitle.text = animal
+            lblTitle.text = emoji
+            navigationItem.title = animal
+            
+        }
+        
+    }
+    
+    func switchAnimal(animal: String) -> String {
+        switch animal {
+        case "Dog":
+            return "🐶"
+        case "Cat":
+            return "🐱"
+        case "Mouse":
+            return "🐭"
+        case "Hamster":
+            return "🐹"
+        case "Bunny":
+            return "🐰"
+        case "Panda":
+            return "🐼"
+        case "Lion":
+            return "🦁"
+        case "Pig":
+            return "🐷"
+        case "Frog":
+            return "🐸"
+        case "Octopus":
+            return "🐙"
+        default:
+            return ""
+        }
+
+    }
+    
+    
 }
