fc51682d182498c98e0f74564bb2785e974cf201
ac <ac@Jaxxs-MBP.home>
2016-12-05 19:31:45 -0500

    completed lab

--- Animals/AnimalViewController.swift
+++ Animals/AnimalViewController.swift
@@ -9,4 +9,38 @@
 import UIKit
 
 class AnimalViewController: UIViewController {
+    
+    @IBOutlet weak var emoji: UILabel!
+    @IBOutlet weak var emojiName: UILabel!
+    
+    
+    var animalName: String?
+    
+    func name2emoji(name:String)->String {
+        switch name {
+            case "Dog": return "🐶"
+            case "Cat": return "🐱"
+            case "Mouse": return "🐭"
+            case "Hamster": return "🐹"
+            case "Bunny": return "🐰"
+            case "Panda": return "🐼"
+            case "Lion": return "🦁"
+            case "Pig": return "🐷"
+            case "Frog": return "🐸"
+            case "Octopus": return "🐙"
+            default: return "☹️"
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if let animalName = animalName {
+                emojiName.text = animalName
+                emoji.text = name2emoji(name: animalName)
+                navigationItem.title = animalName
+        }
+    }
+    
+ 
+        
+    
 }
