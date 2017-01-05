6106bb319685f6d1634b6b76643140a8e4f73e2f
richardsonjm <richardsonjm@gmail.com>
2017-01-05 11:48:49 -0500

    Animals everywhere

--- Animals/AnimalViewController.swift
+++ Animals/AnimalViewController.swift
@@ -9,4 +9,42 @@
 import UIKit
 
 class AnimalViewController: UIViewController {
+    var animal: String?
+    @IBOutlet weak var animalEmojiLabel: UILabel!
+    @IBOutlet weak var animalNameLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if let animal = animal {
+            navigationItem.title = animal
+            animalEmojiLabel?.text = nameToEmoji(animal)
+            animalNameLabel?.text = animal
+        }
+    }
+    
+    fileprivate func nameToEmoji(_ emoji: String) -> String {
+        switch emoji {
+        case "Dog":
+            return "🐶"
+        case "Cat":
+            return "🐱"
+        case "Mouse":
+            return "🐭"
+        case "Hamster":
+            return "🐹"
+        case "Bunny":
+            return "🐰"
+        case "Panda":
+            return "🐼"
+        case "Lion":
+            return "🦁"
+        case "Pig":
+            return "🐷"
+        case "Frog":
+            return "🐸"
+        case "Octopus":
+            return "🐙"
+        default:
+            return ""
+        }
+    }
 }
