d64992d9f2de3e693a48a8217cfb167610b5d724
Ryan Hennings <r.hennings11@gmail.com>
2017-02-17 21:59:22 -0800

    Segues lab complete

--- Animals/AnimalListViewController.swift
+++ Animals/AnimalListViewController.swift
@@ -9,4 +9,51 @@
 import UIKit
 
 class AnimalListViewController: UITableViewController {
+    
+    @IBOutlet var animalTableView: UITableView!
+    var animals: [String] = [
+        "Dog",
+        "Cat",
+        "Mouse",
+        "Hamster",
+        "Bunny",
+        "Panda",
+        "Lion",
+        "Pig",
+        "Frog",
+        "Octopus"
+    ]
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let animalCell = animalTableView.dequeueReusableCell(withIdentifier: "AnimalCell", for: indexPath)
+        
+        let animal = animals[(indexPath as NSIndexPath).row]
+        
+        animalCell.textLabel?.text = animal
+        
+        return animalCell
+    }
+    
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier != "AnimalDetailView" {
+            return
+        }
+        if let dest = segue.destination as? AnimalViewController {
+            
+            if let selectedAnimal = animalTableView.indexPathForSelectedRow {
+                dest.animal = animals[(selectedAnimal as NSIndexPath).row]
+            }
+        }
+    }
 }
--- Animals/AnimalViewController.swift
+++ Animals/AnimalViewController.swift
@@ -9,4 +9,45 @@
 import UIKit
 
 class AnimalViewController: UIViewController {
+    
+    
+    @IBOutlet weak var animalEmojiLabel: UILabel!
+    @IBOutlet weak var animalNameLabel: UILabel!
+    
+    var animal: String = ""
+    
+    override func viewWillAppear(_ animated: Bool) {
+        animalNameLabel.text = animal
+        animalEmojiLabel.text = setAnimalEmoji(animal)
+        
+        navigationItem.title = animal
+        
+    }
+    
+    func setAnimalEmoji(_ name: String) -> String {
+        switch name {
+        case "Dog":
+            return "🐶"
+        case "Cat":
+            return "🐱"
+        case "Mouse":
+            return "🐭"
+        case "Hamster":
+            return "🐹"
+        case "Bunny":
+            return "🐰"
+        case "Panda":
+            return "🐼"
+        case "Lion":
+            return "🦁"
+        case "Pig":
+            return "🐷"
+        case "Frog":
+            return "🐸"
+        case "Octopus":
+            return "🐙"
+        default:
+            return "No Animal Selected"
+        }
+    }
 }
