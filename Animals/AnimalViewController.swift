1e14b6caaaa0f2e78efc454f681cf52956fa3260
othmansafarini <othman_safarini@live.co.uk>
2017-02-21 14:39:13 +0400

    OK,Done

--- Animals/AnimalViewController.swift
+++ Animals/AnimalViewController.swift
@@ -9,4 +9,50 @@
 import UIKit
 
 class AnimalViewController: UIViewController {
+    
+    
+    @IBOutlet weak var EmojiLabel: UILabel!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var animal: String?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let animal = animal {
+    
+            let animalEmoji = nameToEmoji(animal)
+            
+            nameLabel?.text = animal
+            EmojiLabel?.text = animalEmoji
+            navigationItem.title = animal
+        }
+    }
+    
+    fileprivate func nameToEmoji(_ animal: String) -> String {
+        switch animal {
+        case "Dog":
+            return "🐶"
+        case "Cat":
+            return "🐱"
+        case "Mouse":
+            return "🐭"
+        case "Hamster":
+            return "🐹"
+        case "Bunny":
+            return "🐰"
+        case "Panda":
+            return "🐼"
+        case "Lion":
+            return "🦁"
+        case "Pig":
+            return "🐷"
+        case "Frog":
+            return "🐸"
+        case "Octopus":
+            return "🐙"
+        default:
+            return ""
+        }
+    }
 }
