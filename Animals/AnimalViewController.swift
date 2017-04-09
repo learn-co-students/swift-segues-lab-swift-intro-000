42b6a3a172f6ad417d79b597a22d0ca17fd7c954
g3kenny <g3_kenny@hotmail.com>
2017-04-10 00:59:28 +0800

    Table segue lab1

--- Animals/AnimalViewController.swift
+++ Animals/AnimalViewController.swift
@@ -9,4 +9,34 @@
 import UIKit
 
 class AnimalViewController: UIViewController {
+    
+    @IBOutlet var lblEmoticon: UILabel!
+    @IBOutlet var lblName: UILabel!
+    
+    var animal: String?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let animal = animal {
+            lblEmoticon.text = nameToEmoji(name: animal)
+            lblName.text = animal
+            navigationItem.title = animal
+        }
+    }
+    
+    func nameToEmoji(name: String) -> String {
+        switch name {
+        case "Dog":     return "🐶"
+        case "Cat":     return "🐱"
+        case "Mouse":   return "🐭"
+        case "Hamster": return "🐹"
+        case "Bunny":   return "🐰"
+        case "Panda":   return "🐼"
+        case "Lion":    return "🦁"
+        case "Pig":     return "🐷"
+        case "Frog":    return "🐸"
+        case "Octopus": return "🐙"
+        default:        return ""
+        }
+    }
 }
