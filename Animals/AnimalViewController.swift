35f6e58dccc94fcb826cb1c1e04239d086db9084
Hendricus Disseldorp <hdisseldorp@gmail.com>
2017-04-25 12:43:29 +0300

    Tableview and segues

--- Animals/AnimalViewController.swift
+++ Animals/AnimalViewController.swift
@@ -9,4 +9,47 @@
 import UIKit
 
 class AnimalViewController: UIViewController {
+  
+  @IBOutlet weak var emojiLabel: UILabel!
+  @IBOutlet weak var nameLabel: UILabel!
+  
+  var animal: String?
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    if let animal = animal {
+      nameLabel?.text = animal
+      let animalEmoji = nameToEmoji(animal)
+      emojiLabel?.text = animalEmoji
+      navigationItem.title = animal
+    }
+  }
+  
+  fileprivate func nameToEmoji(_ name: String) -> String {
+    switch name {
+    case "Dog":
+      return "🐶"
+    case "Cat":
+      return "🐱"
+    case "Mouse":
+      return "🐭"
+    case "Hamster":
+      return "🐹"
+    case "Bunny":
+      return "🐰"
+    case "Panda":
+      return "🐼"
+    case "Lion":
+      return "🦁"
+    case "Pig":
+      return "🐷"
+    case "Frog":
+      return "🐸"
+    case "Octopus":
+      return "🐙"
+    default:
+      return ""
+    }
+  }
 }
