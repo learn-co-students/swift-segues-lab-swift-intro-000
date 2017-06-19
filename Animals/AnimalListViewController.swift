9a33bdf7166a3e5f9509b90d9901b0d7587cb06e
Morgan McCauley <morgan.mccauley94@gmail.com>
2017-06-19 15:08:00 +1200

    Swift segues lab

--- Animals/AnimalListViewController.swift
+++ Animals/AnimalListViewController.swift
@@ -9,4 +9,44 @@
 import UIKit
 
 class AnimalListViewController: UITableViewController {
+    
+    let animalList = [
+        "Dog",
+        "Cat",
+        "Mouse",
+        "Hamster",
+        "Bunny",
+        "Panda",
+        "Lion",
+        "Pig",
+        "Frog",
+        "Octopus"
+    ];
+
+    override func numberOfSections(in tableView: UITableView) -> Int{
+        return 1;
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animalList.count;
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AnimalCell", for: indexPath)
+        let animal = animalList[indexPath.row]
+        cell.textLabel?.text = animal;
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "AnimalDetailView" else { return; }
+        
+        if let dest = segue.destination as? AnimalViewController,
+            let indexPath = tableView.indexPathForSelectedRow {
+            dest.animal = animalList[(indexPath as NSIndexPath).row];
+        }
+    }
+    
+    
 }
