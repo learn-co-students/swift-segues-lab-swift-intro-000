3f0114124953315f90dfec43b0f7534b1e881db3
Cynthia Nikolai <cnikolai@earthlink.net>
2018-03-28 08:27:35 -0400

    finished exercise

---
+++ Animals/AnimalViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  AnimalViewCell.swift
+//  Animals
+//
+//  Created by Cynthia Nikolai on 3/28/18.
+//  Copyright © 2018 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+   //MARK: Properties
+    
+}
