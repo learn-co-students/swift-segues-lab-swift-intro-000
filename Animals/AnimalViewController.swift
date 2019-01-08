cf33f7f9892ee67a77816ee0fdd809c7a63b595b
jidafi <jidafi@gmail.com>
2019-01-08 11:45:47 -0600

    Completed

--- Animals/AnimalViewController.swift
+++ Animals/AnimalViewController.swift
@@ -9,4 +9,40 @@
 import UIKit
 
 class AnimalViewController: UIViewController {
+    var animalSent = ""
+    
+    @IBOutlet weak var largeLabel: UILabel!
+    @IBOutlet weak var smallLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        smallLabel.text = animalSent
+        largeLabel.text = animalName(animalSent)
+    }
+    
+    func animalName(_ name: String) -> String {
+        switch name {
+        case "Dog":
+            return "🐶"
+        case "Cat":
+            return "🐱"
+        case "Mouse":
+            return "🐭"
+        case "Hamster":
+            return "🐹"
+        case "Bunny":
+            return "🐰"
+        case "Panda":
+            return "🐼"
+        case "Lion":
+            return "🦁"
+        case "Pig":
+            return "🐷"
+        case "Frog":
+            return "🐸"
+        case "Octopus":
+            return "🐙"
+        default:
+            return "Error"
+            }
+    }
 }
